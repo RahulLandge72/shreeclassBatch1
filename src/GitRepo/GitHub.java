5573527c1c76db37fca3ace82082eb36d17dea37
Rahul <www.rahullandge72@gmail.com>
2022-11-22 11:14:12 +0530

    first commit

---
+++ src/GitRepo/GitHub.java
@@ -0,0 +1,21 @@
+package GitRepo;
+
+public class GitHub {
+
+	public void test1() {
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println(c);
+		
+	}
+
+
+		
+	}
+class Git extends GitHub{
+public static void main(String[] args) {
+	GitHub obj=new GitHub();
+	obj.test1();
+	System.out.println("This is Rahul Landge");
+}
+}
